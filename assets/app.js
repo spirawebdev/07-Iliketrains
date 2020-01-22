01e1e5f6040d7e51a61dbab362dccf13f0c311d2
Anthony Dees <spirawebdev@gmail.com>
2020-01-22 16:58:58 -0700

    Firebase Functionality was added

---
+++ assets/app.js
@@ -0,0 +1,35 @@
+// Firebase Setup
+ $(document).ready(function() {
+
+    // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAOjo6FgE4zXlFMsWEeJsiKqiIoyWfkH6g",
+    authDomain: "ireallyliketrains-3f99b.firebaseapp.com",
+    databaseURL: "https://ireallyliketrains-3f99b.firebaseio.com",
+    projectId: "ireallyliketrains-3f99b",
+    storageBucket: "ireallyliketrains-3f99b.appspot.com",
+    messagingSenderId: "1082815632888",
+    appId: "1:1082815632888:web:643789fde8200fd30e320d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ });
